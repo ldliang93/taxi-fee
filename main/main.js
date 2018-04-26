72eefaf09276aa2d7a647538a729f506386cd7c8
ldliang93 <ldliang93@163.com>
2018-04-26 10:50:50 +0800

    test all

--- main/main.js
+++ main/main.js
@@ -1,7 +1,12 @@
-module.exports = function calculatePrice(miles) {
+module.exports = function calculatePrice(input) {
    let price = 6;
-   if(miles > 2){
-     price += 0.8;
+   if(input.miles > 2 && input.miles <= 8){
+     price += 0.8*(input.miles-2);
    }
-   return price;
+   if (input.miles >8) {
+     price += (4.8 + 1.2*(input.miles-8));
+   }
+   price += input.wait*0.25;
+
+   return Math.round(price);
 };
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -2,16 +2,44 @@ const calculatePrice = require('../main/main');
 
 describe('taxi fee', function () {
   it('should return 6 when input 2km', function() {
-    let input = 2;
+    let input = {'miles': 2 , 'wait': 0};
     let summary = calculatePrice(input);
     let expected = 6;
     expect(summary).toEqual(expected)
   });
 
-  it('should return 6.8 when input 3km', function() {
-    let input = 3;
+  it('should return 8 when input 4km', function() {
+    let input = {'miles': 4 , 'wait': 0};;
     let summary = calculatePrice(input);
-    let expected = 6.8;
+    let expected = 8;
+    expect(summary).toEqual(expected)
+  });
+
+  it('should return 12 when input 9km', function() {
+    let input = {'miles': 9 , 'wait': 0};;
+    let summary = calculatePrice(input);
+    let expected = 12;
+    expect(summary).toEqual(expected)
+  });
+
+  it('should return 7 when input 2km wait 2min', function() {
+    let input = {'miles': 2 , 'wait': 2};
+    let summary = calculatePrice(input);
+    let expected = 7;
+    expect(summary).toEqual(expected)
+  });
+
+  it('should return 8 when input 3km wait 4min', function() {
+    let input = {'miles': 3 , 'wait': 4};;
+    let summary = calculatePrice(input);
+    let expected = 8;
+    expect(summary).toEqual(expected)
+  });
+
+  it('should return 12 when input 9km wait 4min', function() {
+    let input = {'miles': 9 , 'wait': 4};;
+    let summary = calculatePrice(input);
+    let expected = 13;
     expect(summary).toEqual(expected)
   });
 });
